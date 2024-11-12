7e6aa23296600a36e44a7d268a3774a1fb858224
IgorAPSantos <igorppandre@hotmail.com>
2024-11-11 22:00:08 -0300

    add styles endPageButton

---
+++ p1/src/components/EndPageButton/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        marginBottom: 50,
+        width: '80%',
+    },
+    button: {
+        backgroundColor: '#5C0098',
+        borderRadius: 30,
+        padding: 15,
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#FFF',
+        textAlign: 'center'
+    }
+}
+)
