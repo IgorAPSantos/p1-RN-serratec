1919949ea7754d7ed3ecee880f681e45f906a43f
IgorAPSantos <igorppandre@hotmail.com>
2024-11-11 21:51:37 -0300

    add setLimit screen no app

--- p1/App.tsx
+++ p1/App.tsx
@@ -1,10 +1,10 @@
 import Home from './src/screens/Home/';
+// import SetLimit from './src/screens/SetLimit';
 
 export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Home />
+    // <SetLimit />
   );
 }
 
